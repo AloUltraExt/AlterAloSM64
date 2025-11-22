1059ebc3a9caf8d187cc2ce09434b76393a4a234
AloXado320 <david.albujar.s.30@gmail.com>
2025-11-21 21:02:14 -0500

    Shiftable Segments 1

--- src/game/memory.c
+++ src/game/memory.c
@@ -114,11 +114,7 @@ void move_segment_table_to_dmem(void) {
  * freeing the object that was most recently allocated from a side.
  */
 void main_pool_init(UNUSED_CN void *start, void *end) {
-#if defined(VERSION_CN) && !defined(USE_EXT_RAM)
-    sPoolStart = (u8 *) ALIGN16((uintptr_t) &gZBufferEnd) + 16;
-#else
     sPoolStart = (u8 *) ALIGN16((uintptr_t) start) + 16;
-#endif
     sPoolEnd = (u8 *) ALIGN16((uintptr_t) end - 15) - 16;
     sPoolFreeSpace = sPoolEnd - sPoolStart;
 
@@ -367,8 +363,8 @@ void *load_segment_decompress_heap(u32 segment, u8 *srcStart, u8 *srcEnd) {
 }
 
 void load_engine_code_segment(void) {
-    void *startAddr = (void *) SEG_ENGINE;
-    u32 totalSize = SEG_FRAMEBUFFERS - SEG_ENGINE;
+    void *startAddr = (void *) _engineSegmentStart;
+    u32 totalSize = _engineSegmentEnd - _engineSegmentStart;
     UNUSED u32 alignedSize = ALIGN16(_engineSegmentRomEnd - _engineSegmentRomStart);
 
     bzero(startAddr, totalSize);
